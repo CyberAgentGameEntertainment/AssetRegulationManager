81e6a0e1bc34884bb672f63593258ee92cf568b1
kaito_nagahama <kaito_nagahama_xa@cyberagent.co.jp>
2021-11-11 17:32:55 +0900

    feat: TextureSize用のレギュレーションを仮実装

---
+++ Assets/AssetRegulationManager/Editor/TextureSizeRegulationEntry.cs
@@ -0,0 +1,24 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace AssetRegulationManager.Editor
+{
+    public class TextureSizeRegulationEntry : IAssetRegulationEntry
+    {
+        [SerializeField] private Vector2 _textureSize;
+        public string Label => "Texture Size";
+        public string Explanation => $"Texture size must be less than({_textureSize.x}x{_textureSize.y})";
+
+        public bool RunTest(string path)
+        {
+            var texture = AssetDatabase.LoadAssetAtPath<Texture2D>(path);
+
+            return texture.width < _textureSize.x && texture.height < _textureSize.y;
+        }
+
+        public void DrawGUI()
+        {
+            // TODO: 実装する
+        }
+    }
+}

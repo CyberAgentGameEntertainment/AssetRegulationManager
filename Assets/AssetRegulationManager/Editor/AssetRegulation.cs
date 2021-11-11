0a801a0c2f483b4cc5a26532143a8c42d605db9c
kaito_nagahama <kaito_nagahama_xa@cyberagent.co.jp>
2021-11-11 17:31:50 +0900

    feat: データとして使うためのScriptableObjectを追加

---
+++ Assets/AssetRegulationManager/Editor/AssetRegulation.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace AssetRegulationManager.Editor
+{
+    [Serializable]
+    public class AssetRegulation
+    {
+        public int id;
+        public string name;
+        public string assetPathRegex;
+        public List<IAssetRegulationEntry> Regulations;
+    }
+}
---
+++ Assets/AssetRegulationManager/Editor/AssetRegulationCollection.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AssetRegulationManager.Editor
+{
+    public class AssetRegulationCollection : ScriptableObject
+    {
+        public List<AssetRegulation> Collection;
+    }
+}

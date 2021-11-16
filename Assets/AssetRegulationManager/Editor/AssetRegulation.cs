2b84ab8565782d84f041e73c29d6cd744233003e
kaito_nagahama <kaito_nagahama_xa@cyberagent.co.jp>
2021-11-16 11:28:07 +0900

    update: entryの実行側でアセットを読み込むように

--- Assets/AssetRegulationManager/Editor/AssetRegulation.cs
+++ Assets/AssetRegulationManager/Editor/AssetRegulation.cs
@@ -1,3 +1,7 @@
+// --------------------------------------------------------------
+// Copyright 2021 CyberAgent, Inc.
+// --------------------------------------------------------------
+
 using System;
 using System.Collections.Generic;
 using UnityEngine;

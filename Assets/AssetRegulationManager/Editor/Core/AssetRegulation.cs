a59062a2243f6ac3707ebfa9c5143c3706980f01
kaito_nagahama <kaito_nagahama_xa@cyberagent.co.jp>
2021-11-17 15:55:42 +0900

    clean: コメントを追加

--- Assets/AssetRegulationManager/Editor/Core/AssetRegulation.cs
+++ Assets/AssetRegulationManager/Editor/Core/AssetRegulation.cs
@@ -4,10 +4,14 @@
 
 using System;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace AssetRegulationManager.Editor.Core
 {
+    /// <summary>
+    ///     AssetRegulation data class.
+    /// </summary>
     [Serializable]
     public class AssetRegulation
     {
@@ -15,27 +19,47 @@ namespace AssetRegulationManager.Editor.Core
         [SerializeField] private string _assetPathRegex;
         [SerializeReference] private List<IAssetRegulationEntry> _entries;
 
-        private string _id;
-
-        public string Id => _id;
+        /// <summary>
+        ///     Initialize.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name "></param>
+        /// <param name="assetPathRegex"></param>
+        /// <param name="entries"></param>
+        public AssetRegulation(string id, string name, string assetPathRegex, List<IAssetRegulationEntry> entries)
+        {
+            Id = id;
+            _name = name;
+            _assetPathRegex = assetPathRegex;
+            _entries = entries;
+        }
+        
+        /// <summary>
+        ///     Regulation management ID.
+        /// </summary>
+        public string Id { get; }
+        
+        /// <summary>
+        ///     Regulation Name.
+        /// </summary>
         public string Name
         {
             set => _name = value;
             get => _name;
         }
+        
+        /// <summary>
+        ///     Regex for paths
+        /// </summary>
         public string AssetPathRegex
         {
             set => _assetPathRegex = value;
             get => _assetPathRegex;
         }
-        public List<IAssetRegulationEntry> Entries => _entries;
         
-        public AssetRegulation(string id, string name, string assetPathRegex, List<IAssetRegulationEntry> entries)
-        {
-            _id = id;
-            _name = name;
-            _assetPathRegex = assetPathRegex;
-            _entries = entries;
-        }
+        /// <summary>
+        ///     Regulation Entry Collection.
+        /// </summary>
+        public List<IAssetRegulationEntry> Entries => _entries;
     }
 }
--- Assets/AssetRegulationManager/Editor/Core/IAssetRegulationEntry.cs
+++ Assets/AssetRegulationManager/Editor/Core/IAssetRegulationEntry.cs
@@ -2,15 +2,35 @@
 // Copyright 2021 CyberAgent, Inc.
 // --------------------------------------------------------------
 
-using Object = UnityEngine.Object;
+using UnityEngine;
 
 namespace AssetRegulationManager.Editor.Core
 {
+    /// <summary>
+    ///     The base class of the Asset Regulation Entry.
+    /// </summary>
     public interface IAssetRegulationEntry
     {
+        /// <summary>
+        ///     Label for GUI.
+        /// </summary>
         string Label { get; }
+        
+        /// <summary>
+        ///     Regulation explanation.
+        /// </summary>
         string Explanation { get; }
+        
+        /// <summary>
+        ///     call AssetRegulationEntry<TAsset>.RunTest(TAsset asset) by assigning obj.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         bool RunTest(Object obj);
+        
+        /// <summary>
+        ///    Draw the regulations in the GUI.
+        /// </summary>
         void DrawGUI();
     }
 }

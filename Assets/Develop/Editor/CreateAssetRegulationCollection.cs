1d9dd6926b8f4c3000b9596d6c90c1bcaa2b9336
kaito_nagahama <kaito_nagahama_xa@cyberagent.co.jp>
2021-11-18 13:12:12 +0900

    feat: develop用に追加

---
+++ Assets/Develop/Editor/CreateAssetRegulationCollection.cs
@@ -0,0 +1,30 @@
+// --------------------------------------------------------------
+// Copyright 2021 CyberAgent, Inc.
+// --------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using AssetRegulationManager.Editor.Core;
+using UnityEditor;
+using UnityEngine;
+
+namespace Develop.Editor
+{
+    public class CreateAssetRegulationCollection
+    {
+        [MenuItem("Develop/CreateAssetRegulationCollection")]
+        private static void CreateScriptableObject()
+        {
+            var assetRegulationCollection = ScriptableObject.CreateInstance<AssetRegulationCollection>();
+            assetRegulationCollection.Regulations.Add(new AssetRegulation(Guid.NewGuid().ToString(), "rule1",
+                "Assets/Develop/Textures",
+                new List<IAssetRegulationEntry> {new TextureSizeRegulationEntry(new Vector2(128, 128))}));
+            var fileName = "AssetRegulationCollection.asset";
+            var path = "Assets/Develop";
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            AssetDatabase.CreateAsset(assetRegulationCollection, Path.Combine(path, fileName));
+        }
+    }
+}

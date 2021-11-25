b79f10621fe95507a7398195d98e7f1c9ee9d08f
kaito_nagahama <kaito_nagahama_xa@cyberagent.co.jp>
2021-11-25 10:37:10 +0900

    clean: textureを最初に読み込むように変更

--- Assets/AssetRegulationManager/Editor/Core/Viewer/RegulationTreeView.cs
+++ Assets/AssetRegulationManager/Editor/Core/Viewer/RegulationTreeView.cs
@@ -4,7 +4,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using AssetRegulationManager.Editor.Foundation.EasyTreeView;
 using UnityEditor;
@@ -13,13 +12,19 @@ using UnityEngine;
 
 namespace AssetRegulationManager.Editor.Core.Viewer
 {
-    internal sealed class RegulationTreeView : TreeViewBase
+    internal class RegulationTreeView : TreeViewBase
     {
+        private readonly Texture2D _testFailedTexture;
+        private readonly Texture2D _testNormalTexture;
+        private readonly Texture2D _testPassedTexture;
+        private int _currentId;
+
         internal RegulationTreeView(TreeViewState treeViewState) : base(treeViewState)
         {
+            _testPassedTexture = EditorGUIUtility.Load("TestPassed") as Texture2D;
+            _testFailedTexture = EditorGUIUtility.Load("TestFailed") as Texture2D;
+            _testNormalTexture = EditorGUIUtility.Load("TestNormal") as Texture2D;
         }
-
-        private int _currentId = 0;
 
         protected override IOrderedEnumerable<TreeViewItem> OrderItems(IList<TreeViewItem> items, int keyColumnIndex,
             bool ascending)
@@ -34,31 +39,33 @@ namespace AssetRegulationManager.Editor.Core.Viewer
 
         internal AssetPathTreeViewItem AddAssetPathTreeViewItem(string path)
         {
-            var assetPathTreeViewItem = new AssetPathTreeViewItem()
+            var assetPathTreeViewItem = new AssetPathTreeViewItem
             {
                 id = ++_currentId,
                 displayName = path
             };
-            
+
             AddItemAndSetParent(assetPathTreeViewItem, -1);
 
             return assetPathTreeViewItem;
-        }        
-        
-        internal AssetRegulationTreeViewItem AddAssetRegulationTreeViewItem(RegulationEntryDatum entryDatum, int parentId)
+        }
+
+        internal AssetRegulationTreeViewItem AddAssetRegulationTreeViewItem(RegulationEntryDatum entryDatum,
+            int parentId)
         {
-            var assetRegulationTreeViewItem = new AssetRegulationTreeViewItem(entryDatum.MetaDatum, entryDatum.Explanation, entryDatum.ResultType.Value)
+            var assetRegulationTreeViewItem = new AssetRegulationTreeViewItem(entryDatum.MetaDatum,
+                entryDatum.Explanation, entryDatum.ResultType.Value)
             {
                 id = ++_currentId,
                 displayName = entryDatum.Explanation
             };
-            
+
             AddItemAndSetParent(assetRegulationTreeViewItem, parentId);
 
             return assetRegulationTreeViewItem;
         }
-        
-        protected override void RowGUI (RowGUIArgs args)
+
+        protected override void RowGUI(RowGUIArgs args)
         {
             var resultType = TestResultType.None;
             if (args.item is AssetPathTreeViewItem assetPathTreeViewItem)
@@ -70,25 +77,24 @@ namespace AssetRegulationManager.Editor.Core.Viewer
                 if (regulationTreeViewItems.Any(x => x.ResultType == TestResultType.Failed))
                     resultType = TestResultType.Failed;
             }
+
             if (args.item is AssetRegulationTreeViewItem regulationTreeViewItem)
-            {
                 resultType = regulationTreeViewItem.ResultType;
-            }
 
             var texture = resultType switch
             {
-                TestResultType.Success => EditorGUIUtility.Load("TestPassed") as Texture2D,
-                TestResultType.Failed => EditorGUIUtility.Load("TestFailed") as Texture2D,
-                _ => EditorGUIUtility.Load("TestNormal") as Texture2D,
+                TestResultType.Success => _testPassedTexture,
+                TestResultType.Failed => _testFailedTexture,
+                _ => _testNormalTexture
             };
-            
-            Rect toggleRect = args.rowRect;
+
+            var toggleRect = args.rowRect;
             toggleRect.x += GetContentIndent(args.item);
             toggleRect.width = 16f;
             GUI.DrawTexture(toggleRect, texture);
-                
+
             extraSpaceBeforeIconAndLabel = toggleRect.width + 2f;
-            
+
             base.RowGUI(args);
         }
 
@@ -102,18 +108,15 @@ namespace AssetRegulationManager.Editor.Core.Viewer
             return rootItem.children.Select(SearchAssetRegulationTreeViewItem).SelectMany(x => x);
         }
 
-        private IEnumerable<AssetRegulationTreeViewItem> SearchAssetRegulationTreeViewItem(TreeViewItem treeViewItem)
+        protected virtual IEnumerable<AssetRegulationTreeViewItem> SearchAssetRegulationTreeViewItem(
+            TreeViewItem treeViewItem)
         {
             var assetRegulationTreeViewItems = new List<AssetRegulationTreeViewItem>();
 
             if (treeViewItem is AssetPathTreeViewItem)
-            {
                 return treeViewItem.children.OfType<AssetRegulationTreeViewItem>();
-            }
             if (treeViewItem is AssetRegulationTreeViewItem assetRegulationTreeViewItem)
-            {
                 assetRegulationTreeViewItems.Add(assetRegulationTreeViewItem);
-            }
 
             return assetRegulationTreeViewItems;
         }

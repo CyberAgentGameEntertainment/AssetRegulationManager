b4ef90506aba051caaa4491960285ec350a0be49
Haruki Yano <haruki.y.11g@gmail.com>
2022-01-04 22:33:14 +0900

    Update ListableProperty.

--- Assets/AssetRegulationManager/Editor/Foundation/ListableProperty/NonGenericListableProperties.cs
+++ Assets/AssetRegulationManager/Editor/Foundation/ListableProperty/NonGenericListableProperties.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------
-// Copyright 2021 CyberAgent, Inc.
+// Copyright 2022 CyberAgent, Inc.
 // --------------------------------------------------------------
 
 using System;
@@ -197,6 +197,18 @@ namespace AssetRegulationManager.Editor.Foundation.ListableProperty
     /// </summary>
     [Serializable]
     public sealed class ObjectListableProperty : ListableProperty<Object>
+    {
+    }
+
+    /// <summary>
+    ///     <para>Non-generic <see cref="UnityEngine.GameObject" /> type implementation of <see cref="ListableProperty{T}" />.</para>
+    ///     <para>
+    ///         If you use a version of Unity older than 2019, Unity cannot serialize generic type.
+    ///         So you need to use this non-generic class.
+    ///     </para>
+    /// </summary>
+    [Serializable]
+    public sealed class GameObjectListableProperty : ListableProperty<GameObject>
     {
     }
 }

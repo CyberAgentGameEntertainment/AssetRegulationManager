7ee15adfa9355e03ca355c638a4afb373b90b0cd
Haruki Yano <haruki.y.11g@gmail.com>
2022-01-04 22:49:16 +0900

    Update ListableProperty.

--- Assets/AssetRegulationManager/Editor/Foundation/ListableProperty/NonGenericListableProperties.cs
+++ Assets/AssetRegulationManager/Editor/Foundation/ListableProperty/NonGenericListableProperties.cs
@@ -211,4 +211,19 @@ namespace AssetRegulationManager.Editor.Foundation.ListableProperty
     public sealed class GameObjectListableProperty : ListableProperty<GameObject>
     {
     }
+
+    /// <summary>
+    ///     <para>
+    ///         Non-generic <see cref="UnityEngine.MonoBehaviour" /> type implementation of
+    ///         <see cref="ListableProperty{T}" />.
+    ///     </para>
+    ///     <para>
+    ///         If you use a version of Unity older than 2019, Unity cannot serialize generic type.
+    ///         So you need to use this non-generic class.
+    ///     </para>
+    /// </summary>
+    [Serializable]
+    public sealed class MonoBehaviourListableProperty : ListableProperty<MonoBehaviour>
+    {
+    }
 }

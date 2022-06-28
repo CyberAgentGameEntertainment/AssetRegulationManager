be265f000f280661b4477d11ef258f37e4823473
Haruki Yano <haruki.y.11g@gmail.com>
2022-06-28 15:25:17 +0900

    Add MaterialShaderKeywordConstraint.

---
+++ Assets/AssetRegulationManager/Editor/Core/Tool/AssetRegulationEditor/AssetConstraintDrawer/MaterialShaderKeywordConstraintDrawer.cs
@@ -0,0 +1,31 @@
+﻿using AssetRegulationManager.Editor.Core.Model.AssetRegulations.AssetConstraintImpl;
+using AssetRegulationManager.Editor.Foundation.CustomDrawers;
+using AssetRegulationManager.Editor.Foundation.ListableProperty;
+using UnityEditor;
+
+namespace AssetRegulationManager.Editor.Core.Tool.AssetRegulationEditor.AssetConstraintDrawer
+{
+    [CustomGUIDrawer(typeof(MaterialShaderKeywordConstraint))]
+    internal class MaterialShaderKeywordConstraintDrawer : GUIDrawer<MaterialShaderKeywordConstraint>
+    {
+        private ListablePropertyGUI<string> _assetPathGUI;
+
+        public override void Setup(object target)
+        {
+            base.Setup(target);
+            _assetPathGUI = new TextListablePropertyGUI("Shader Keyword", Target.Keywords);
+        }
+
+        protected override void GUILayout(MaterialShaderKeywordConstraint target)
+        {
+            using (new EditorGUI.DisabledScope(!target.Keywords.IsListMode))
+            {
+                target.Condition =
+                    (MaterialShaderKeywordConstraint.CheckCondition)EditorGUILayout.EnumPopup("Check Condition",
+                        target.Condition);
+            }
+
+            _assetPathGUI.DoLayout();
+        }
+    }
+}

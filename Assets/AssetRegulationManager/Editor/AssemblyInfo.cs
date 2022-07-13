97bedebab4b7e255c5cf40d7c04da46f1a1a5ce1
Haruki Yano <haruki.y.11g@gmail.com>
2022-07-13 11:53:51 +0900

    Fix asmdef name.

--- Assets/AssetRegulationManager/Editor/AssemblyInfo.cs
+++ Assets/AssetRegulationManager/Editor/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("AsserRegulationManager.Tests.Editor")]
+[assembly: InternalsVisibleTo("AssetRegulationManager.Tests.Editor")]
